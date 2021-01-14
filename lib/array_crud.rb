522a908230e3323580c5a6e3bd54de3050ca524b
Jonathan Block <blockjonathan29@gmail.com>
2021-01-14 12:03:44 -0500

    commit tree 671b3991788da771651e7285459a8e6e06276635

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ ["Johnny", "Moira", "David", "Alexis"] 
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["Johnny", "Moira", "David", "Alexis"] 
+  array.push(element)
 end
+
+
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["Johnny", "Moira", "David", "Alexis", "Mutt"]
+  array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array)
-  
+
+def remove_element_from_end_of_array(array) 
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(array, index_number)3
+  return array[index_number]
 end
+
 
 def retrieve_first_element_from_array(array)
-  
+  return array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(array) 
+  return array[5]
 end
